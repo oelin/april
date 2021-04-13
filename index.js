9cdf675a9927de0114e8b50556d2aa0faaadf4d5
Oelin <42823429+Oelin@users.noreply.github.com>
2021-04-13 16:18:33 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
 let text = ''
-let use = s => text = s
 
 
-let lex = token => {
+function use() {
+  text = s
+}
+
+
+function lex(token) {
   if (m = text.match(token)) return cut(m)
   throw ''
 }
 
 
-let peek = (...a) => {
+function peek(...a) {
   let s = text
   let node = skip(...a)
   use(s)
@@ -17,7 +21,7 @@ let peek = (...a) => {
 }
 
 
-let skip = (f, ...a) => {
+funciton skip(f, ...a) {
   let s = text
 
   try {
@@ -34,8 +38,6 @@ let cut = m => {
   text = text.slice(value.length)
   return value
 }
-
-
 
 
 module.exports = {
