53fd0532297c2947d49b012c05845c69ecb5cf09
Oelin <42823429+Oelin@users.noreply.github.com>
2023-01-17 12:11:13 +0000

    Update test.js

--- tests/test.js
+++ tests/test.js
@@ -1,3 +1,6 @@
+// April is so small that GitHub doesn't seem to recognise it as "being written in JavaScript"
+// This is currently a placeholder file so JavaScript actually shows up on GitHub.
+
 var someFunction = function(){
 	return 1;
 }
